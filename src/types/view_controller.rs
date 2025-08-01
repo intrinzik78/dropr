c6c4c1138bad2d486ae9e874f5879b828526859b
Joshua Briggs <gitlab@urbanwarzonepaintball.com>
2025-08-01 07:37:37 -0500

    View controller display methods chainable
    Methods
    + header
    + print
    + println
    + prompt_public (placeholder)
    + prompt_private (placeholder)

--- src/types/view_controller.rs
+++ src/types/view_controller.rs
@@ -1,18 +1,51 @@
+use std::io::{ stdout, Write };
 use crate::enums::Error;
 
 type Result<T> = std::result::Result<T,Error>;
 
 #[derive(Debug)]
-pub struct View {
-
-}
+pub struct View;
 
 impl View {
     pub fn new() -> Result<View> {
-        let view = View {
-            
-        };
+        let view = View {};
 
         Ok(view)
+    }
+
+    pub fn header(&self, m: &str) -> Result<&View> {
+        println!("\n{}",m);
+
+        for _ in m.chars() {
+            print!("-");
+        }
+
+        print!("\n");
+        stdout().flush().map_err(|_| Error::FailedToFlushStdOut)?;
+
+        Ok(&self)
+    }
+
+    pub fn end(&self) {
+        println!("");
+    }
+
+    pub fn print(&self, m: &str) -> Result<&View> {
+        print!("{}",m);
+        stdout().flush().map_err(|_| Error::FailedToFlushStdOut)?;
+        Ok(&self)
+    }
+
+    pub fn println(&self, m: &str) -> &View {
+        println!("{}",m);
+        &self
+    }
+
+    pub fn prompt_public(&self, _m: &str) -> Result<String> {
+        todo!()
+    }
+
+    pub fn prompt_private(&self, _m: &str) -> Result<String> {
+        todo!()
     }
 }
